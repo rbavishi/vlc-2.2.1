f7a3157e8d303657b4eb0b9dac3d4ffe3ac30268
Rbavishi <rohan.bavishi95@gmail.com>
2015-07-26 11:53:19 +0530

    Repo-ready

--- subtitle_extract.py
+++ subtitle_extract.py
@@ -1,6 +1,7 @@
 import pysrt, sys
 import easygui
 from operator import *
+import nltk
 def parse_time(time_str):
   ind_times=time_str.split(':')
 
@@ -21,13 +22,36 @@ def parse_time(time_str):
   return { 'hours': hours, 'minutes': minutes, 'seconds': seconds }
 
 def refine_text(string_arg):
-  temp=string_arg.replace('...','$#%')
+  temp=string_arg.replace('\n',' ')
+  temp=temp.replace('...','$#%')
   if(temp[-1]!='.'):
     temp+=' '
   temp=temp.replace('.','.\n')
   temp=temp.replace('$#%','...')
   return temp
 
+def default_string(corpus):
+  tokens=nltk.word_tokenize(corpus)
+  tagged=nltk.pos_tag(tokens)
+  text_corpus=nltk.Text(tokens)
+  dates=[i for i in tagged if i[1]=='CD']
+  dates=[i for i in dates if (i[0].isdigit() and len(i[0])==4)]
+  year=""
+  main_topic=""
+  if len(dates)==1:
+    year=dates[0][0]
+  elif len(dates)>1:
+    freqdict=nltk.FreqDist(text_corpus);
+    max_freq=max([freqdict[i[0]] for i in dates])
+    candidates=[i for i in dates if freqdict[i[0]]==max_freq]
+    year=candidates[0][0]
+
+  main_topic=text_corpus.collocations()
+  if main_topic!="":
+    main_topic=main_topic.split(';')[0]
+  if year!="":
+    year+=" - "
+  return year+main_topic
 
 if __name__ == "__main__":
   arguments=sys.argv[1:]
@@ -37,9 +61,11 @@ if __name__ == "__main__":
   extract=subs.slice(starts_after=start_time, ends_before=end_time)
   refined_extract=[refine_text(str(i.text)) for i in extract]
   if(len(refined_extract)!=0):
-    filename=easygui.enterbox('Enter Filename (without extension): ', 'Subtitle-Split', 'Subtitle-Extract')
+    corpus=reduce(add, refined_extract)
+    prefix=arguments[0][:(arguments[0].rfind('/')+1)]
+    filename=easygui.enterbox('Enter Filename (without extension): ', 'Subtitle-Split', default_string(corpus))
     main_string_extract=reduce(add, refined_extract)
-    f=open(filename+'.txt', 'a')
+    f=open(prefix+filename+'.txt', 'a')
     f.write(str(main_string_extract)+'\n')
     f.close()
 
